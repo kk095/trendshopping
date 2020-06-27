f7ff7c0e5a3bbefc546c1c300043f5301473b523
kk <kk41110002@gmail.com>
2020-06-27 19:19:51 +0530

    try to solve static problem

--- myweb/settings.py
+++ myweb/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
+
+STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'media'),
     ]
 
